d527352832905cc43627ba91b276cc20be1d4c15
quangnmwork <quangnm.work@gmail.com>
2023-02-14 16:11:43 +0700

    play with mock API

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,37 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import React from "react";
+import { rest } from "msw";
+import { setupServer } from "msw/node";
+import { render, fireEvent, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
+import "@testing-library/jest-dom";
+import Fetch from "./Fetch";
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+const server = setupServer(
+  rest.get("/greeting", (req, res, ctx) => {
+    return res(ctx.json({ greeting: "hello there" }));
+  })
+);
+beforeAll(() => server.listen());
+afterEach(() => server.resetHandlers());
+afterAll(() => server.close());
+test("loads and displays greeting", async () => {
+  render(<Fetch url={"/greeting"} />);
+  fireEvent.click(screen.getByText("Load Greeting"));
+  await screen.findByRole("heading");
+  expect(screen.getByRole("heading")).toBeDefined();
+  expect(screen.getByRole("button")).toBeDisabled();
+});
+
+test("handles server error", async () => {
+  server.use(
+    rest.get("/greeting", (req, res, ctx) => {
+      return res(ctx.status(500));
+    })
+  );
+  render(<Fetch url={"/greeting"} />);
+  userEvent.click(screen.getByText("Loading Greeting"));
+
+  await screen.findByRole("alert");
+  expect(screen.getByRole("alert")).toHaveTextContent("Oops, failed to fetch!");
+  expect(screen.getByRole("button")).not.toBeDisabled();
 });
